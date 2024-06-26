c0f3d52eb1c266458ebdb4f850ba6d28866e9f66
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2024-06-26 17:42:03 -0400

    fixing

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,17 +1,9 @@
 export const exerciseOptions = {
-  //   method: 'GET',
- 
-  //   headers: {
-  //       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        
-  //     'X-RapidAPI-Key': 'b014123293mshae648029c50c277p136984jsnb80d71732f1f',
-  //   }
-  // }
+
   method: 'GET',
 
   headers: {
-    // 'X-RapidAPI-Key': 'b014123293mshae648029c50c277p136984jsnb80d71732f1f',
-    // 'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+
    'x-rapidapi-host': 'exercisedb.p.rapidapi.com' ,
    'x-rapidapi-key': 'e9726a31aemsh7bbe54858538333p1ccfa7jsn4fc85a8cf509'
   }
@@ -20,11 +12,7 @@ export const youtubeOptions = {
   method: 'GET',
 
   headers: {
-    // 'X-RapidAPI-Key': 'b014123293mshae648029c50c277p136984jsnb80d71732f1f',
-
-    // 'X-RapidAPI-Key': 'b014123293mshae648029c50c277p136984jsnb80d71732f1f',
-    // 'X-RapidAPI-Key': 'b014123293mshae648029c50c277p136984jsnb80d71732f1f',
-    // 'API-key':'AIzaSyDFOB31BkDuyaUiVH-cW5-_mwjg8z2q-24',
+    
 
     'x-rapidapi-key': 'e9726a31aemsh7bbe54858538333p1ccfa7jsn4fc85a8cf509',
 
@@ -32,7 +20,7 @@ export const youtubeOptions = {
 
 
 
-    // 'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+
   }
 };
 
