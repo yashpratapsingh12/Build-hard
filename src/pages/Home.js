4f9163fef38a8077fe26b9a4119c73214b3cf297
Yash pratap singh <151184047+yashpratapsingh12@users.noreply.github.com>
2024-07-14 16:35:32 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,6 @@ import Exercises from '../components/Exercises'
 const Home = () => {
   const[bodyPart,setBodyPart]= useState('all')
   const [exercises,setExercises]= useState([]);
-  console.log(bodyPart);
   return (
 <Box>
   <HeroBanner/>
